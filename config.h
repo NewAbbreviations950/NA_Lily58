0bd38150cad79a2ac99fa77ffeff201ac13d355b
Avex <104692825+NewAbbreviations950@users.noreply.github.com>
2024-04-10 20:30:13 +0200

    Update config.h

--- config.h
+++ config.h
@@ -26,7 +26,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  #define MASTER_RIGHT
 // #define EE_HANDS
 
-#define TAPPING_TOGGLE 2
+#define TAPPING_TOGGLE 3
 #define TAPPING_TERM 200
 
 //#undef RGBLED_NUM
