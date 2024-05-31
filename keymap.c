167ee61506ec85d31c42f3b927e29f7eb7288f34
Avex <104692825+NewAbbreviations950@users.noreply.github.com>
2024-05-31 22:46:05 +0200

    weak oled attribute

--- keymap.c
+++ keymap.c
@@ -287,6 +287,12 @@ bool process_record_keymap(uint16_t keycode, keyrecord_t *record) { return true;
 __attribute__((weak))
 bool process_record_secrets(uint16_t keycode, keyrecord_t *record) { return true; }
 
+#ifdef OLED_ENABLE
+__attribute__((weak))
+bool process_record_oled(uint16_t keycode, keyrecord_t *record) { return true; }
+#endif
+
+
 /*declare boolean for jiggler*/
 bool is_jiggling = false;
 
