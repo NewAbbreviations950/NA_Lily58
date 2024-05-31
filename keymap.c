f2be147c0b8b22a5a91173e8624d66f74ac7e2bf
Avex <104692825+NewAbbreviations950@users.noreply.github.com>
2024-05-31 23:05:26 +0200

    Update keymap.c

--- keymap.c
+++ keymap.c
@@ -455,7 +455,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             break;
     }
     
-    return process_record_keymap(keycode, record) && process_record_secrets(keycode, record);
+    return process_record_keymap(keycode, record) && process_record_secrets(keycode, record)
             #ifdef OLED_ENABLE
             && process_record_oled(keycode, record)
             #endif
